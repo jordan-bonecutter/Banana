aab8721503c1415bfccaf68f87970df69a36a88c
Jordan Bonecutter <jordanbonecutter@gmail.com>
2020-08-10 16:49:47 -0700

    added new int range function

--- banana.h
+++ banana.h
@@ -15,6 +15,7 @@ banana_random_engine* banana_random_engine_create_with_timed_seed();
 void banana_random_engine_destroy(banana_random_engine* engine);
 
 int   banana_random_engine_next_int(banana_random_engine* engine);
+int   banana_random_engine_next_int_in_range(banana_random_engine* engine, int lo, int hi);
 double banana_random_engine_next_float(banana_random_engine* engine);
 double banana_random_engine_next_float_in_range(banana_random_engine* engine, float lo, float hi);
 double banana_random_engine_next_gaussian(banana_random_engine* engine, float mean, float std_dev);
--- banana_random_engine.c
+++ banana_random_engine.c
@@ -66,6 +66,26 @@ int banana_random_engine_next_int(banana_random_engine* engine){
   return y; 
 }
 
+int banana_random_engine_next_int_in_range(banana_random_engine* engine, int lo, int hi){
+  int n_random_bits, range, ret, mask;
+
+  if(hi <= lo){
+    return 0;
+  }
+
+  range = hi - lo;
+  for(n_random_bits = 0; range > 0; ++n_random_bits, range >>= 1);
+  range = hi - lo;
+  mask = 0xffffffff>>(32 - n_random_bits);
+  
+  for(ret = range + 1; ret > range;){
+    ret = banana_random_engine_next_int(engine);
+    ret &= mask;
+  }
+
+  return ret + lo;
+}
+
 /* genereate a 32 bit int using MT and normalize it */
 double banana_random_engine_next_float(banana_random_engine* engine){
   unsigned int nextint = banana_random_engine_next_int(engine);
@@ -95,24 +115,23 @@ double banana_random_engine_next_gaussian(banana_random_engine* engine, float me
 
 #ifdef BANANA_RANDOM_ENGINE_UNIT_TEST
 
-#define TESTITERS 1000000
+#define TESTITERS 10000000
+#define RANGE     10
 #include <stdio.h>
+#include <limits.h>
 
 int main(){
-  banana_random_engine* engine = banana_random_engine_create_with_timed_seed();
-  int array[100] = {0};
-  int i;
-  double result;
+  banana_random_engine* engine = banana_random_engine_create();
+  int array[RANGE] = {0};
+  int i, array_pos;
+  unsigned result;
 
   for(i = 0; i < TESTITERS; ++i){
-    result = banana_random_engine_next_gaussian(engine, 0.5, 0.2);
-    if(result < 0 || result > 1){
-      continue;
-    }
-    ++array[(int)(result*100)];
+    result = banana_random_engine_next_int_in_range(engine, 0, RANGE-1);
+    ++array[result];
   }
 
-  for(i = 0; i < 100; ++i){
+  for(i = 0; i < RANGE; ++i){
     printf("%d\n", array[i]);
   }
 
